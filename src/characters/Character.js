1d5593ffee5919543b6c482c9f2c7f120591e95a
wheatv3015 <60979035+wheatv3015@users.noreply.github.com>
2020-10-05 12:59:44 -0500

    Add files via upload

---
+++ src/characters/Character.js
@@ -0,0 +1,51 @@
+import Phaser from 'phaser'
+
+class Character {
+  constructor (baseDamage, baseDefense, currentHealth, maxHealth, baseInitative) {
+    this.Attack = baseDamage
+    this.Defense = baseDefense
+    this.HP = currentHealth
+    this.MaxHP = maxHealth
+    this.Initiative = baseInitative
+  }
+
+  getAttack () {
+    return this.Attack
+  }
+
+  getDefense () {
+    return this.Defense
+  }
+
+  getHP () {
+    return this.HP
+  }
+
+  getMaxHP () {
+    return this.MaxHP
+  }
+
+  getInitiative () {
+    return this.Initiative
+  }
+
+  takeDamage (damageToTake) {
+    if (damageToTake - this.Defense > 0) {
+      this.HP = this.HP - (damageToTake - this.Defense)
+    }
+    if (this.HP <= 0) {
+      // destroy this character
+    }
+  }
+
+  heal (amountToHeal) {
+    if (amountToHeal + this.HP >= this.MaxHP) {
+      this.HP = this.MaxHP
+    }
+    else {
+      this.HP = this.HP + amountToHeal
+    }
+  }
+}
+
+export default Character
