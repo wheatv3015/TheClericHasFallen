823d7f6589ef878afe4c977c13d98f2e1d043bb3
Katherine <60979280+KatherineRude@users.noreply.github.com>
2020-10-07 11:57:37 -0500

    MushStats
    Added a Mush and gave it stats

---
+++ src/characters/Mushroom.js
@@ -0,0 +1,19 @@
+import Phaser from 'phaser'
+
+import Character from './Character.js'
+
+class Mushroom extends Character {
+    super () {
+        this.Attack = CONFIG.MUSH_BASE_ATTACK
+        this.Defense = CONFIG.MUSH_BASE_DEFENSE
+        this.HP = CONFIG.MUSH_MAX_HEALTH
+        this.MaxHP = CONFIG.MUSH_MAX_HEALTH
+        this.Initiative = CONFIG.MUSH_INITIATIVE
+        this.Mode = CONFIG.MUSH_MODE
+      }
+
+
+      //if (Mode == 0) Defensive Mode
+
+      //if (Mode == 1) Agressive Mode
+}
