4091b1ac561ce83f366666f7d1083125c2ba9b45
Katherine <60979280+KatherineRude@users.noreply.github.com>
2020-10-07 12:04:11 -0500

    FixedSpacing
    Fixes spacing on Mush

--- src/characters/Mushroom.js
+++ src/characters/Mushroom.js
@@ -2,18 +2,19 @@ import Phaser from 'phaser'
 
 import Character from './Character.js'
 
+import CONFIG from '../config.js'
+
 class Mushroom extends Character {
-    super () {
-        this.Attack = CONFIG.MUSH_BASE_ATTACK
-        this.Defense = CONFIG.MUSH_BASE_DEFENSE
-        this.HP = CONFIG.MUSH_MAX_HEALTH
-        this.MaxHP = CONFIG.MUSH_MAX_HEALTH
-        this.Initiative = CONFIG.MUSH_INITIATIVE
-        this.Mode = CONFIG.MUSH_MODE
-      }
+  super () {
+    this.Attack = CONFIG.MUSH_BASE_ATTACK
+    this.Defense = CONFIG.MUSH_BASE_DEFENSE
+    this.HP = CONFIG.MUSH_MAX_HEALTH
+    this.MaxHP = CONFIG.MUSH_MAX_HEALTH
+    this.Initiative = CONFIG.MUSH_INITIATIVE
+    this.Mode = CONFIG.MUSH_MODE
+  }
 
+   //if (Mode == 0) Defensive Mode
 
-      //if (Mode == 0) Defensive Mode
-
-      //if (Mode == 1) Agressive Mode
+   //if (Mode == 1) Agressive Mode
 }
